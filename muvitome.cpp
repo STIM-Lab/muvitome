acfe0f8a0f38314ccf85231f73842c384fe6e1ec
m-bluhm <maximillian.bluhm@gmail.com>
2023-07-13 15:31:25 -0500

    final test build

--- muvitome.cpp
+++ muvitome.cpp
@@ -389,7 +389,7 @@ void RenderUI() {
 
             //MAX ADDED, check the automatic mode checkbox if want use the automated system I've been developing
             if (ImGui::Checkbox("Automatic mode?", &automatic_mode)) {
-                // MAX: set up any variables here to get it started
+                light_changed = 1;
                 
             }
 
