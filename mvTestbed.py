6346bdd8204f2a71ebe4d8361f7f3b5188910d96
David Mayerich <mayerich@uh.edu>
2023-08-31 14:57:09 -0500

    added comments to the testbed file

--- mvTestbed.py
+++ mvTestbed.py
@@ -9,29 +9,37 @@ import matplotlib.pyplot as plt
 import skimage
 import numpy as np
 
+# specify the source and destination directory
+# (note that the destination directory will by default contain several copies of the files)
+
 source_directory = "D:/2023_08_24_16_56_35_david_rebooted"
 dest_directory = "D:/2023_08_24_16_56_35_david_rebooted_processed"
-
 if not os.path.exists(dest_directory):
     os.mkdir(dest_directory)
 
 #%%
 # FLATFIELD CORRECTION
+# This code calculates a flatfield image for correction by averaging all images in the source directory
+# The flatfield image is stored in the destination directory as a bitmap
 
 F = mvf.AverageMosaic(source_directory, extension="bmp")
 skimage.io.imsave(dest_directory + "/flatfield.bmp", F.astype(np.uint8))
 
 
 #%%
+# This code performs flatfield correction by loading the flatfield image and correcting all images in the source directory
+# This creates a separate directory in destination called "flat"
 F = skimage.io.imread(dest_directory + "/flatfield.bmp")
-
 mvf.FlatfieldMosaic(source_directory, dest_directory + "/flat", gain = 1, flatfield_image=F, extension="bmp")
 
 #%%
-# assemble the mosaic image
+# BUILD MOSAIC
+# This code assembles all of the corrected images into a mosaic stored in the "mosaic" directory"
 MosaicFolder = dest_directory + "/mosaic"
 mvm.AssembleMuvitome(dest_directory + "/flat", MosaicFolder, (10, 0))
 
 #%%
+# ALIGN IMAGES
+# This code aligns the images and stores the result as a new image stack in "aligned"
 offsets = mva.CalculateImageOffsetListY(dest_directory + "/flat/00002_00004", N=100, sigma=3)
 mva.ApplyImageOffsetsY(dest_directory + "/mosaic", dest_directory + "/aligned", offsets)
